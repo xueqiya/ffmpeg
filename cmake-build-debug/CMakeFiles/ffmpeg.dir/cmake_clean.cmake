58dbdf09e6e5c18f1e21c64b3e8cd627e04baa9c
xueqi <jiaowoxueqiya@gmail.com>
2021-10-22 14:49:31 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/ffmpeg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ffmpeg.dir/main.cpp.o"
+  "ffmpeg"
+  "ffmpeg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ffmpeg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
